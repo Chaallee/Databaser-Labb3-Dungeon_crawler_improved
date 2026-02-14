296479d8cfae1f3a19741e9a10fc15a2d3d04d0b
Charlie Carlegrund <charlie_carlegrund@hotmail.com>
2026-02-14 17:46:10 +0100

    playerclass

---
+++ Labb02_Dungeon_crawler/Database/PlayerClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb02_Dungeon_crawler.Database
+{
+    public class PlayerClass
+    {
+        public string Name { get; set; }
+    }
+}
