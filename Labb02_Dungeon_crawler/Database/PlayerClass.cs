b0a05995482922637646e5c2f230824f9da8edab
Charlie Carlegrund <charlie_carlegrund@hotmail.com>
2026-02-14 19:01:26 +0100

    Database/playerclass

--- Labb02_Dungeon_crawler/Database/PlayerClass.cs
+++ Labb02_Dungeon_crawler/Database/PlayerClass.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Labb02_Dungeon_crawler.Database
 {
     public class PlayerClass
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
         public string Name { get; set; }
     }
 }
